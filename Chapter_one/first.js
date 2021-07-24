1ecd8db979e2c4f8627052bee15459c96c67b7e2
Torjuman <torjumanrimon7@gmail.com>
2021-07-24 11:15:19 +0600

    Some testing console log have been tested.

--- Chapter_one/first.js
+++ Chapter_one/first.js
@@ -2,3 +2,9 @@ console.log('Hello World !')
 
 console.log('Hello, my name is Torjuman Rimon.')
 console.log('JavaScript is a awsome language.')
+
+console.log('This a string value')
+
+console.log(655454)
+
+console.log('This is called int = ', 8446)
