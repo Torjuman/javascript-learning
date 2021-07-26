ba5f3f7dfb8dbd8a750b87d42cdfdd7b49f80581
Torjuman <torjumanrimon7@gmail.com>
2021-07-26 21:24:22 +0600

    Try simple if else condition

---
+++ Chapter_one_intro/ifElseCondition.js
@@ -0,0 +1,18 @@
+
+var a = 80
+var b = 30
+
+if (a>b) {
+    console.log(a + ' is greater than ' + b)
+} else {
+    console.log(a + ' is smaller than ' + b)
+}
+
+
+var n = 100
+
+if (n % 2 === 0) {
+    console.log(n + ' is an even number.')
+} else {
+    console.log(n + ' is an odd number.')
+}
