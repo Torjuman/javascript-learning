09f1f8398a45e2378499931ad35327697c65c09e
Torjuman <torjumanrimon7@gmail.com>
2021-07-26 13:02:06 +0600

    Learn about if condition

---
+++ Chapter_one_intro/ifCondition.js
@@ -0,0 +1,20 @@
+var a = 10
+var b = 5
+
+if (a > b) {
+    console.log(a + ' is greater than ' + b)
+}
+
+if (a < b) {
+    console.log(a + ' is smaller than ' + b)
+}
+
+var x = 99
+
+if (x % 2 === 0) {
+    console.log(x + ' is an even number.')
+}
+
+if (x % 2 === 1) {
+    console.log(x + ' is an odd number.')
+}
