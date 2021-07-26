8b6c56f612623c1807deed76d6459a966cc4da41
Torjuman <torjumanrimon7@gmail.com>
2021-07-26 21:41:30 +0600

    Try if else if condition

---
+++ Chapter_one_intro/ifElseIfCondition.js
@@ -0,0 +1,21 @@
+
+var a = 75
+var b = 75
+
+if (a > b) {
+    console.log(a + ' is greater than ' + b)
+} else if (a < b) {
+    console.log(a + ' is smaller than ' + b)
+} else {
+    console.log(a + ' and ' + b + ', they both are same.')
+}
+
+var n = 0
+
+if (n === 0) {
+    console.log(n + ', this is a zero')
+} else if (n % 2 === 0) {
+    console.log(n + ' is an even number.')
+} else {
+    console.log(n + ' is an odd number.')
+}
