7e10e4a1c91c081ef5673421632856f0c3e03b0b
Torjuman <torjumanrimon7@gmail.com>
2021-07-28 16:10:06 +0600

    Sum of even number

--- Chapter_two_loops/forLoops.js
+++ Chapter_two_loops/forLoops.js
@@ -1,21 +1,21 @@
 // First time try for loop
-// for (var i = 0; i < 10; i+= 2) {
-//     console.log((i + 1) + ' Torjuman Rimon')
-// }
+for (var i = 0; i < 10; i+= 2) {
+    console.log((i + 1) + ' Torjuman Rimon')
+}
 
-// for (var i = 0; i <= 10; i++) {
-//     console.log(i)
-// }
+for (var i = 0; i <= 10; i++) {
+    console.log(i)
+}
 
 // Print Even and Odd Number
 
-// for (var i = 1; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i + ' is an Even number')
-//     } else {
-//         console.log(i + ' is an Odd number')
-//     }
-// }
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i + ' is an Even number')
+    } else {
+        console.log(i + ' is an Odd number')
+    }
+}
 
 // Sum of 1 to 10
 var sum = 0
@@ -29,5 +29,16 @@ var nSum = 0
 
 for (var i = 1; i <= 10; i++) {
     console.log(nSum + ' + ' + i + ' = ' + (nSum + i))
-    nSum +=i
+    nSum += i
 }
+
+// Sum of 1 to 10 even number
+
+var evenSum = 0
+
+for (var i = 1; i <=10; i++) {
+    if (i % 2 === 0) {
+        console.log(evenSum + ' + ' + i + ' = ' + (evenSum + i))
+        evenSum += i
+    }
+} 
