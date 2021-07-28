3fea79ccf97db620343b0084cce4d64b74196b52
Torjuman <torjumanrimon7@gmail.com>
2021-07-28 15:59:27 +0600

    Sum of n th number useing for loop

--- Chapter_two_loops/forLoops.js
+++ Chapter_two_loops/forLoops.js
@@ -1,15 +1,33 @@
-for (var i = 0; i < 10; i+= 2) {
-    console.log((i + 1) + ' Torjuman Rimon')
-}
+// First time try for loop
+// for (var i = 0; i < 10; i+= 2) {
+//     console.log((i + 1) + ' Torjuman Rimon')
+// }
 
-for (var i = 0; i <= 10; i++) {
-    console.log(i)
-}
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// Print Even and Odd Number
+
+// for (var i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + ' is an Even number')
+//     } else {
+//         console.log(i + ' is an Odd number')
+//     }
+// }
+
+// Sum of 1 to 10
+var sum = 0
 
 for (var i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i + ' is an Even number')
-    } else {
-        console.log(i + ' is an Odd number')
-    }
+    sum += i
+    console.log(sum)
+}
+
+var nSum = 0
+
+for (var i = 1; i <= 10; i++) {
+    console.log(nSum + ' + ' + i + ' = ' + (nSum + i))
+    nSum +=i
 }
