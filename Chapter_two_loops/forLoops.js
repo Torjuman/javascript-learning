8d9f957414b6cdbe6f6e2722fba130a2e9f8bbe7
Torjuman <torjumanrimon7@gmail.com>
2021-07-28 15:49:20 +0600

    Even and Odd number are checked useing for loop and if condition

--- Chapter_two_loops/forLoops.js
+++ Chapter_two_loops/forLoops.js
@@ -5,3 +5,11 @@ for (var i = 0; i < 10; i+= 2) {
 for (var i = 0; i <= 10; i++) {
     console.log(i)
 }
+
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i + ' is an Even number')
+    } else {
+        console.log(i + ' is an Odd number')
+    }
+}
