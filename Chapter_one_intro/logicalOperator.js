ac3ef8014e5ea1ba7a84c60186dcb580e30fcbdb
Torjuman <torjumanrimon7@gmail.com>
2021-07-28 12:40:11 +0600

    Try or operator(||)

--- Chapter_one_intro/logicalOperator.js
+++ Chapter_one_intro/logicalOperator.js
@@ -7,10 +7,22 @@
 var a = 30
 var b = 20
 var c = 40
-var d = 10
+var d = 50
 
 if (a > b && c > d) {
     console.log('a is greater than b and  c is greater than d.')
 } else {
     console.log('At least one condition should be true')
+}
+
+// A || B
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+if (a > b || c > d) {
+    console.log('a is greater than b and  c is greater than d.')
+} else {
+    console.log('Both condition are false.')
 }
