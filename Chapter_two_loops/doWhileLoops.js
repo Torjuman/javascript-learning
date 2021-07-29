e7348429e9443ba9d9c0928ac89a532d3fe17a1d
Torjuman <torjumanrimon7@gmail.com>
2021-07-29 13:02:01 +0600

    Learn about do while loop

---
+++ Chapter_two_loops/doWhileLoops.js
@@ -0,0 +1,10 @@
+var isRunning = false
+
+while (isRunning) {
+    console.log('I am running from while loop...')
+}
+
+// do while execute the code remain in do block even if the condition is false
+do {
+    console.log('I am running from do while loop...')
+} while (isRunning)
