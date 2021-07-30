3c3fc86bd3f27aea229e62d16eb8010ce58588d3
Torjuman <torjumanrimon7@gmail.com>
2021-07-30 20:09:02 +0600

    Compare the string

---
+++ Chapter_three_string/stringComparison.js
@@ -0,0 +1,15 @@
+
+var a = 'abc'
+var b = 'abd'
+
+// All small letter are greater then All Capital
+var c = 'ABC'
+
+console.log(a < b)
+console.log(a < c)
+
+// In the time of comparison with string number and number, it will turn into number value.
+var n = '001'
+var n1 = 1
+
+console.log(n == n1)
