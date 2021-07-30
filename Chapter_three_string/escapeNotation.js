84f283dd4e18363412efa295b9909aaefa745d99
Torjuman <torjumanrimon7@gmail.com>
2021-07-30 19:59:51 +0600

    Learn escape Notation

---
+++ Chapter_three_string/escapeNotation.js
@@ -0,0 +1,10 @@
+var str = 'This is a \'String\''
+console.log(str)
+
+// For new line
+var str2 = 'This is \na new line'
+console.log(str2)
+
+// For tab space
+var str3 = 'There are some\tTab'
+console.log(str3)
