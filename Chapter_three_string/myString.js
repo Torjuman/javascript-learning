666e0dabde484274c8f65cb1e7c1e3c3ec44c075
Torjuman <torjumanrimon7@gmail.com>
2021-07-30 19:51:16 +0600

    Learn Sting

---
+++ Chapter_three_string/myString.js
@@ -0,0 +1,14 @@
+// String declar in between single qout.
+
+var str = 'My name is Torjuman Rimon.'
+console.log(str)
+
+// String declar in between double qout.
+
+var str2 = "I am 25 years old."
+console.log(str2)
+
+// String declar in constructor way
+
+var str3 = String('I am from Bangladesh.')
+console.log(str3)
