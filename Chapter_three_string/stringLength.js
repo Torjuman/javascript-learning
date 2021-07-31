8e0f8400bfa1293fd173d088c6ff2aa51808825a
Torjuman <torjumanrimon7@gmail.com>
2021-07-31 15:37:19 +0600

    Make my own string property

---
+++ Chapter_three_string/stringLength.js
@@ -0,0 +1,12 @@
+var str = 'Torjuman Rimon'
+var myLength = 0
+
+while (true) {
+    if (str.charAt(myLength) == '') {
+        break
+    } else {
+        myLength++
+    }
+}
+console.log(myLength)
+console.log(str.length)
