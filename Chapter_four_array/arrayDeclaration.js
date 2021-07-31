82ff67036280151fdcfe3bc4ee02a5ea52fa4317
Torjuman <torjumanrimon7@gmail.com>
2021-07-31 18:12:12 +0600

    Array Declaration

---
+++ Chapter_four_array/arrayDeclaration.js
@@ -0,0 +1,17 @@
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(arr)
+
+// Check data from certain index
+console.log(arr[5])
+
+// Insert any data into array
+arr[10] = 15
+console.log(arr)
+
+// Check an array length
+console.log(arr.length)
+
+// Declar an array in constructor form
+var arr2 = Array(9, 8, 7, 6, 5, 4)
+console.log(arr2)
