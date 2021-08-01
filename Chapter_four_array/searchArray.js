416c942377ca5c19e2c5e09ceb4b49a2296d1bca
Torjuman <torjumanrimon7@gmail.com>
2021-08-01 08:05:41 +0600

    Add some line of extra code

--- Chapter_four_array/searchArray.js
+++ Chapter_four_array/searchArray.js
@@ -1,15 +1,27 @@
 var arr = [5, 6, 2, 77, 10, 23, 78]
+var arr2 = [5, 6, 2, 77, 23, 78]
 var find = 10
-var isFound = false
+var isFoundF = false
+var isFoundS = false
 
 for (var i = 0; i < arr.length; i++) {
     if (arr[i] === find) {
-        console.log('Data is found at ' + i + 'th number index.')
-        isFound = true
+        console.log('Data is found at first Array at ' + i + 'th number index.')
+        isFoundF = true
+        break;
+    }
+}
+for (var i = 0; i < arr2.length; i++) {
+    if (arr2[i] === find) {
+        console.log('Data is found at second Array at ' + i + 'th number index.')
+        isFoundS = true
         break;
     }
 }
 
-if (!isFound) {
-    console.log('Data is not found.')
+if (!isFoundF) {
+    console.log('Data is not found at first Array.')
+}
+if (!isFoundS) {
+    console.log('Data is not found at second Array.')
 }
