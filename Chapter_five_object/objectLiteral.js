a7ed7d251c3948aab86b4eabf6a9f19bae02e697
Torjuman <torjumanrimon7@gmail.com>
2021-08-02 07:44:24 +0600

    Learn about Object

---
+++ Chapter_five_object/objectLiteral.js
@@ -0,0 +1,28 @@
+var obj = {
+    name: 'Torjuman Rimon',
+    age: 25,
+    studyStatus: true
+}
+console.log(obj)
+
+// Insert value from out side of an Object
+var point = {
+    a: 10,
+    b: 20,
+}
+point.c = 30
+point.d = 40
+
+console.log(point)
+
+// Object Constructor
+var obj2 = Object()
+obj2.x = 90
+obj2.y = 95
+console.log(obj2)
+
+var obj3 = new Object()
+obj3.m = 75
+obj3.n = 85
+
+console.log(obj3)
