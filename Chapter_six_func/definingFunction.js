331beae4b25c3241150afe81991b9e6005c8e7d4
Torjuman <torjumanrimon7@gmail.com>
2021-08-03 07:25:21 +0600

    Learn about function and function calling

---
+++ Chapter_six_func/definingFunction.js
@@ -0,0 +1,25 @@
+
+function add () {
+    var a = 20
+    var b = 10
+    console.log(a + b)
+}
+add()
+function sub () {
+    var a = 20
+    var b = 10
+    console.log(a - b)
+}
+sub()
+function divs () {
+    var a = 20
+    var b = 10
+    console.log(a / b)
+}
+divs()
+function multiply () {
+    var a = 20
+    var b = 10
+    console.log(a * b)
+}
+multiply()
