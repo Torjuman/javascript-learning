1e79d45a9d57b244b3dba17c7d39c2919b4a0231
Torjuman <torjumanrimon7@gmail.com>
2021-08-05 13:01:07 +0600

    Return a funtion from another funtion

--- Chapter_seven_func_js/higherOrderFunction.js
+++ Chapter_seven_func_js/higherOrderFunction.js
@@ -25,3 +25,17 @@ console.log(newMulti())
 
 // We can return a function in an another function.
 
+function divide(a, b, func) {
+
+    // We can also make an anonymous function.
+    return function () {
+        var s = func(a, b)
+        var c = 5
+        return s / c
+    }
+
+}
+
+var newDivide = divide(a, b, addition)
+console.log(newDivide)
+console.log(newDivide())
