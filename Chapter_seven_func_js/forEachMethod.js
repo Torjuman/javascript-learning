6beb75286797be62548754481ce51753fec96220
Torjuman <torjumanrimon7@gmail.com>
2021-08-06 17:01:00 +0600

    Update code

--- Chapter_seven_func_js/forEachMethod.js
+++ Chapter_seven_func_js/forEachMethod.js
@@ -11,16 +11,16 @@ arr.forEach(function (value, index, array) {
 // It will return a new Array
 console.log(arr)
 
-// // Implement my own forEach function
+// Implement my own forEach function
 
-// function myForEach(arr, cb) {
-//     for (var i = 0; i < arr.length; i++) {
-//         // This code is responsible for value, index, array
-//         cb(arr[i], i, arr)
-//     }
-// }
+function myForEach(arr, cb) {
+    for (var i = 0; i < arr.length; i++) {
+        // This code is responsible for value, index, array
+        cb(arr[i], i, arr)
+    }
+}
 
-// var arr2 = [5, 3, 9]
-// myForEach(arr2, function (value, index, array) {
-//     console.log(value, index, array)
-// })
+var arr2 = [5, 3, 9]
+myForEach(arr2, function (value, index, array) {
+    console.log(value, index, array)
+})
