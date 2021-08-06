45f646cec5267fad38e324cfc74e1cb3af23d84a
Torjuman <torjumanrimon7@gmail.com>
2021-08-06 16:58:34 +0600

    code modify

--- Chapter_seven_func_js/forEachMethod.js
+++ Chapter_seven_func_js/forEachMethod.js
@@ -3,24 +3,24 @@ var arr = [7, 5, 3, 1]
 
 console.log(arr)
 
-var st = arr.forEach(function (value, index, array) {
+arr.forEach(function (value, index, array) {
     // console.log(value, index, array)
-    array[index] = value * 5
+    array[index] = value + 3
 })
 
 // It will return a new Array
 console.log(arr)
 
-// Implement my own forEach function
+// // Implement my own forEach function
 
-function myForEach(arr, cb) {
-    for (var i = 0; i < arr.length; i++) {
-        // This code is responsible for value, index, array
-        cb(arr[i], i, arr)
-    }
-}
+// function myForEach(arr, cb) {
+//     for (var i = 0; i < arr.length; i++) {
+//         // This code is responsible for value, index, array
+//         cb(arr[i], i, arr)
+//     }
+// }
 
-var arr2 = [5, 3, 9]
-myForEach(arr2, function (value, index, array) {
-    console.log(value, index, array)
-})
+// var arr2 = [5, 3, 9]
+// myForEach(arr2, function (value, index, array) {
+//     console.log(value, index, array)
+// })
