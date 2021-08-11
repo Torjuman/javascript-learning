14e8f3b3cfa9aedc9f131e8d2c0e425615da8580
Torjuman <torjumanrimon7@gmail.com>
2021-08-11 21:55:04 +0600

    make a function recursive to sum of an array

--- Chapter_seven_func_js/recursiveFunction.js
+++ Chapter_seven_func_js/recursiveFunction.js
@@ -38,4 +38,15 @@ function factNum(n) {
 
 console.log(factNum(4))
 
+var arr = [1, 2, 3, 4, 5]
 
+function sumOfArray(arr, lastIndex) {
+
+    if (lastIndex < 0) {
+        return 0
+    }
+
+    return arr[lastIndex] + sumOfArray(arr, lastIndex - 1)
+}
+
+console.log(sumOfArray(arr, arr.length - 1))
