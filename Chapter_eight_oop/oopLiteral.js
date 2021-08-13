8e73c03a42661acd3463380de14cb7b1bb06593d
Torjuman <torjumanrimon7@gmail.com>
2021-08-13 20:16:26 +0600

    Step into OOP

---
+++ Chapter_eight_oop/oopLiteral.js
@@ -0,0 +1,17 @@
+
+var rect = {
+
+    // Here we can declare all the properties which an object can have.
+    // First we have to name a key then assign its value.
+
+    width: 100,
+    hiegth: 50,
+
+    // We can also define a function in Object.
+    calculateArea: function () {
+        console.log('I am a rectangle from rect Object....')
+    }
+
+}
+
+rect.calculateArea()
