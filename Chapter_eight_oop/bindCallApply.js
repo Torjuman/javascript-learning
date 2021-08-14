21e68ab005564e7a9c85d5886d8b4985f61e67da
Torjuman <torjumanrimon7@gmail.com>
2021-08-14 23:29:49 +0600

    learn about call method

---
+++ Chapter_eight_oop/bindCallApply.js
@@ -0,0 +1,35 @@
+
+function myTest() {
+
+    // In a pure function 'this' will be refereed to window object.
+    console.log(this)
+
+    // If we use 'this' key word it will be associated with window and return undifined.
+    // If we try to plus undifined with undifined it will return NaN
+    console.log(this.a + this.b)
+
+}
+
+myTest()
+
+function myTest2() {
+
+    console.log(this)
+    console.log(this.a + this.b)
+
+}
+// If we call the function with 'call' method we can provide an object out side of pure function.
+myTest2.call({})
+
+// Now we can provide properties in the object
+myTest2.call({a: 15, b: 12})
+
+function myTest3(c, d) {
+
+    console.log(this)
+    console.log(this.a + this.b + c + d)
+
+}
+
+// We can provide arguments as well.
+myTest3.call({a: 15, b: 12}, 25, 32)
