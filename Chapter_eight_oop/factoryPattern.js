96680cda2d0df489f421b3ad20120bad4a2c4eb1
Torjuman <torjumanrimon7@gmail.com>
2021-08-14 09:25:38 +0600

    make a factory pattern function

---
+++ Chapter_eight_oop/factoryPattern.js
@@ -0,0 +1,29 @@
+
+// Define a function which will work like class.
+// It will be defined in factory pattern.
+// It will reduce code duplication and we can use this function as much we want.
+// It can be maintained Object Oriented Programming theory.
+
+function createRectangle(width, height) {
+    return {
+        width: width,
+        height: height,
+
+        printF: function () {
+            console.log(this)
+
+            // To get the properties value from this function we don't have to use 'this' key word.
+            console.log('My width is ' + width)
+            console.log('My height is ' + height)
+        }
+    }
+}
+
+var rect1 = createRectangle(12, 10)
+rect1.printF()
+
+var rect2 = createRectangle(15, 8)
+rect2.printF()
+
+var rect3 = createRectangle(20, 7)
+rect3.printF()
